772bf048d2ca2973c59c86aced56541a6c894d56
Felipe-Dutra <felipedutra98@gmail.com>
2020-05-02 07:28:22 -0300

    service

--- src/main/java/com/example/curso/resources/CategoriaResource.java
+++ src/main/java/com/example/curso/resources/CategoriaResource.java
@@ -3,24 +3,26 @@ package com.example.curso.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.domain.Categoria;
+import com.example.curso.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio" );
-		
-		List<Categoria> lista = new ArrayList<Categoria>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="{id}",method = RequestMethod.GET)
+	public ResponseEntity<?> find(Integer id) {
+	
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
